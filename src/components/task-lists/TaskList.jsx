0f3d7b2d4cc2b0609abf08ab0f606b2d8743b87d
bbarriatos <bbarriatos@gmail.com>
2022-12-15 14:46:10 +0800

    setting up the route

--- src/components/task-lists/TaskList.jsx
+++ src/components/task-lists/TaskList.jsx
@@ -5,19 +5,23 @@ import "./TaskList.css";
 const TaskList = ({ tasks }) => {
   return (
     <div className="task-lists">
-      {tasks.map((task) => (
-        <Link key={task.id} to={`/${task.id}`} className="task">
-          <p>
-            <strong>ID: </strong> {task.id}
-          </p>
-          <p>
-            <strong>Title: </strong> {task.title}
-          </p>
-          <p>
-            <strong>Status: </strong> {String(task.completed)}
-          </p>
-        </Link>
-      ))}
+      {tasks.length === 0 ? (
+        <h2>No available task</h2>
+      ) : (
+        tasks.map((task) => (
+          <Link key={task.id} to={`/${task.id}`} className="task">
+            <p>
+              <strong>ID: </strong> {task.id}
+            </p>
+            <p>
+              <strong>Title: </strong> {task.title}
+            </p>
+            <p>
+              <strong>Status: </strong> {String(task.completed)}
+            </p>
+          </Link>
+        ))
+      )}
     </div>
   );
 };
